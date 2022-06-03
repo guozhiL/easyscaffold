0e66d4be308574927be013784b26c296836ef8df
guozhi <lgz471488465@163.com>
2022-06-03 23:16:17 +0800

    lib dependencies init

--- app/src/main/java/com/guozhi/easy_scaffold/MainActivity.kt
+++ app/src/main/java/com/guozhi/easy_scaffold/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.guozhi.easy_saffold
+package com.guozhi.easy_scaffold
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.guozhi.easyscaffold.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
